ad50c1110f461a4ea773b5244c0bd7427145bf56
axtk <axtkx@inbox.ru>
2021-03-17 05:38:09 +0300

    added ImmutableStore

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export {default as Store} from '@axtk/store';
+export * from '@axtk/store';
 export {default as useStore} from './src/useStore';
 export {default as StoreProvider} from './src/StoreProvider';
 export {default as StoreContext} from './src/StoreContext';
--- src/useStore.js
+++ src/useStore.js
@@ -1,19 +1,19 @@
 import {useContext, useState, useEffect} from 'react';
-import Store from '@axtk/store';
+import {BasicStore} from '@axtk/store';
 import StoreContext from './StoreContext';
 
 /**
- * @param {number | string | Store} store - a key of a store in StoreProvider or a Store class instance
- * @returns {Store}
+ * @param {number | string | Store | ImmutableStore} store - a key of a store in StoreProvider or a store instance
+ * @returns {Store | ImmutableStore}
  */
 export default store => {
     let stores = useContext(StoreContext);
     let [storeRevision, setStoreRevision] = useState();
 
-    store = store instanceof Store ? store : (stores && stores[store]);
+    store = store instanceof BasicStore ? store : (stores && stores[store]);
 
     useEffect(() => {
-        if (store instanceof Store) {
+        if (store instanceof BasicStore) {
             return store.onUpdate(() => {
                 setStoreRevision(store.getRevision());
             });
