ce46640411bb844d09f139ea6b100fba8225f239
axtk <axtkx@inbox.ru>
2021-03-24 02:52:23 +0300

    useStore watch argument

--- src/useStore.js
+++ src/useStore.js
@@ -2,23 +2,27 @@ import {useContext, useState, useEffect} from 'react';
 import {BasicStore} from '@axtk/store';
 import StoreContext from './StoreContext';
 
+const isStore = x => x instanceof BasicStore;
+const watchRevision = store => isStore(store) ? store.getRevision() : undefined;
+
 /**
  * @param {number | string | Store | ImmutableStore} store - a key of a store in StoreProvider or a store instance
+ * @param {function | null} [watch] - changes in the returned value of this function will cause re-renders
  * @returns {Store | ImmutableStore}
  */
-export default store => {
+export default (store, watch = watchRevision) => {
     let stores = useContext(StoreContext);
-    let [storeRevision, setStoreRevision] = useState();
+    store = isStore(store) ? store : (stores && stores[store]);
 
-    store = store instanceof BasicStore ? store : (stores && stores[store]);
-
+    let setWatchedValue = useState(watch == null ? undefined : watch(store))[1];
     useEffect(() => {
-        if (store instanceof BasicStore) {
+        if (watch != null && isStore(store)) {
             return store.onUpdate(() => {
-                setStoreRevision(store.getRevision());
+                setWatchedValue(watch(store));
             });
         }
-    }, [store]);
+    }, [store, watch]);
 
     return store;
 };
+
