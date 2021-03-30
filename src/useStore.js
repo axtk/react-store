9378e3803e19a15daffcfa6e38d34fc074bc6434
axtk <axtkx@inbox.ru>
2021-03-30 03:21:52 +0300

    store validation in useStore

--- src/useStore.js
+++ src/useStore.js
@@ -3,20 +3,30 @@ import {BasicStore} from '@axtk/store';
 import StoreContext from './StoreContext';
 
 const isStore = x => x instanceof BasicStore;
-const watchRevision = store => isStore(store) ? store.getRevision() : undefined;
+const watchRevision = store => store.getRevision();
 
 /**
  * @param {number | string | Store | ImmutableStore} store - a key of a store in StoreProvider or a store instance
- * @param {function | null} [watch] - changes in the returned value of this function will cause re-renders
+ * @param {function | null} [watch] - the hook will cause a re-render if the returned value of this function changes
  * @returns {Store | ImmutableStore}
  */
 export default (store, watch = watchRevision) => {
-    let stores = useContext(StoreContext);
-    store = isStore(store) ? store : (stores && stores[store]);
+    let contextStores = useContext(StoreContext);
+
+    if (!isStore(store)) {
+        if (!contextStores)
+            throw new Error('No stores in the store context.');
+
+        if (!isStore(contextStores[store]))
+            throw new Error(`Not a store: ${store}`);
+
+        store = contextStores[store];
+    }
 
     let setWatchedValue = useState(watch == null ? undefined : watch(store))[1];
+
     useEffect(() => {
-        if (watch != null && isStore(store)) {
+        if (watch != null) {
             return store.onUpdate(() => {
                 setWatchedValue(watch(store));
             });
@@ -25,4 +35,3 @@ export default (store, watch = watchRevision) => {
 
     return store;
 };
-
