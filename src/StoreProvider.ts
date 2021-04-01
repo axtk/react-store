264a9b0dd8be766327021ab0745eb90e439e78ca
axtk <axtkx@inbox.ru>
2021-04-01 23:22:33 +0300

    improved typing

--- src/StoreProvider.ts
+++ src/StoreProvider.ts
@@ -1,12 +1,11 @@
-import {createElement, ReactNode, ReactElement} from 'react';
+import {createElement, FC} from 'react';
 import {StoreContext} from './StoreContext';
 import {StoreCollection} from './types';
 
 export type StoreProviderProps = {
     stores?: StoreCollection;
-    children?: ReactNode;
 }
 
-export const StoreProvider = ({stores, children}: StoreProviderProps): ReactElement => {
+export const StoreProvider: FC<StoreProviderProps> = ({stores, children}) => {
     return createElement(StoreContext.Provider, {value: stores || {} as StoreCollection}, children);
 };
