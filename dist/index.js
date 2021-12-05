eefce5013e5b8eeae7cd6e0046884fbf450bbd00
axtk <axtkx@inbox.ru>
2021-12-05 23:19:17 +0300

    minor renaming

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,23 @@
 import { useState, useEffect } from 'react';
 export * from '@axtk/store';
-export function useStoreListener(store, updateTriggerSelector = (store => store.getRevision()), equal = Object.is) {
-    let [value, setValue] = useState(updateTriggerSelector === null ? undefined : updateTriggerSelector(store));
+export function useStoreListener(store, selectUpdateTrigger = (store => store.getRevision()), equal = Object.is) {
+    let [value, setValue] = useState(selectUpdateTrigger === null ? undefined : selectUpdateTrigger(store));
     useEffect(() => {
-        // Setting the `updateTriggerSelector` parameter to `null` will prevent the
+        // Setting the `selectUpdateTrigger` parameter to `null` will prevent the
         // specific component from listening to any updates of the specific store.
-        if (updateTriggerSelector !== null) {
+        if (selectUpdateTrigger !== null) {
             return store.onUpdate(() => {
                 // This handler sets a certain value to the component state whenever the
                 // store gets updated in order to trigger a component re-render if the
                 // update trigger value has changed.
                 // By default, the value used as a component update trigger is the store's
                 // revision (which is incremented each time the store data gets updated),
-                // and providing a custom `updateTriggerSelector` function allows to reduce
+                // and providing a custom `selectUpdateTrigger` function allows to reduce
                 // the frequency of the component updates.
-                const nextValue = updateTriggerSelector(store);
+                const nextValue = selectUpdateTrigger(store);
                 if (!equal(value, nextValue))
                     setValue(nextValue);
             });
         }
-    }, [store, updateTriggerSelector, value, setValue]);
+    }, [store, selectUpdateTrigger, value, setValue]);
 }
--- index.ts
+++ index.ts
@@ -8,17 +8,17 @@ export function useStoreListener<
     TypedKeyPathDepth extends number = 5
 >(
     store: Store<State, TypedKeyPathDepth>,
-    updateTriggerSelector: ((store: Store<State, TypedKeyPathDepth>) => any) | null = (store => store.getRevision()),
+    selectUpdateTrigger: ((store: Store<State, TypedKeyPathDepth>) => any) | null = (store => store.getRevision()),
     equal: (prev: any, next: any) => boolean = Object.is,
 ): void {
     let [value, setValue] = useState(
-        updateTriggerSelector === null ? undefined : updateTriggerSelector(store)
+        selectUpdateTrigger === null ? undefined : selectUpdateTrigger(store)
     );
 
     useEffect(() => {
-        // Setting the `updateTriggerSelector` parameter to `null` will prevent the
+        // Setting the `selectUpdateTrigger` parameter to `null` will prevent the
         // specific component from listening to any updates of the specific store.
-        if (updateTriggerSelector !== null) {
+        if (selectUpdateTrigger !== null) {
             return store.onUpdate(() => {
                 // This handler sets a certain value to the component state whenever the
                 // store gets updated in order to trigger a component re-render if the
@@ -26,11 +26,11 @@ export function useStoreListener<
 
                 // By default, the value used as a component update trigger is the store's
                 // revision (which is incremented each time the store data gets updated),
-                // and providing a custom `updateTriggerSelector` function allows to reduce
+                // and providing a custom `selectUpdateTrigger` function allows to reduce
                 // the frequency of the component updates.
-                const nextValue = updateTriggerSelector(store);
+                const nextValue = selectUpdateTrigger(store);
                 if (!equal(value, nextValue)) setValue(nextValue);
             });
         }
-    }, [store, updateTriggerSelector, value, setValue]);
+    }, [store, selectUpdateTrigger, value, setValue]);
 }
